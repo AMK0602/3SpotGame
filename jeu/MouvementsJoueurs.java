72b70b549b6c58ef5482cbede5e68c243afc9020
Thanoux1204 <ehouy92380@gmail.com>
2024-02-22 15:37:58 +0100

    Passage tableau 3 entrée

--- jeu/MouvementsJoueurs.java
+++ jeu/MouvementsJoueurs.java
@@ -6,14 +6,24 @@ import structure.Joueur;
 import java.util.Scanner;
 
 public class MouvementsJoueurs {
+    private static int[][][] listMouvement; // Tableau des combinaisons possibles.
     private static int[][] mouvementsPossibles;
     private static int[][] combiPossible;
     private static int[] indices; // DEFINIR UNE TAILLE AVEC UN COMPTEUR ?
     private static int compteurDeMvt= 0;
+
+    public static boolean combiExist(EtatCase[][] table, int x1, int y1, int x2, int y2, Joueur joueur){
+        int lim_x = table.length;
+        int lim_y = table[0].length;
+
+        if((((x1 >= 0 && x1<lim_x) && (y1>=0 && y1<lim_y)) && ((x2>=0 && x2<lim_x)&&(y2>=0 && y2<lim_y))) && table[x1][y1] == EtatCase.LIBRE && (table[x2][y2] == EtatCase.LIBRE || table[x2][y2] == joueur.getColor())){
+            return true;
+        }
+        return false;
+    }
     public static boolean isMvtPossible(EtatCase[][] table, int ligne, int colonne, Joueur joueur) {
         int taill_ligne = table.length;
         int taille_col = table[0].length;
-
         /**
          *   0 1 2
          * 0 x x x
@@ -22,15 +32,12 @@ public class MouvementsJoueurs {
          * si i(ligne)=0 et j(colonne)=0
          * ( (0>=0 & 0<2 & col>=0 & col<2) &&(00=LIBRE || 00=j.COLOR) && (10=LIBRE || 10=j.COLOR) )
          */
+        //TODO simplifier la vérification en isMvtExist > verifie la case adjacente si EtatCase=LIBRE ou COULEUR
         if (ligne >= 0 && ligne < taill_ligne - 1 && colonne >= 0 && colonne < taille_col &&
                 (table[ligne][colonne] == EtatCase.LIBRE || table[ligne][colonne] == joueur.getColor()) &&
                 (table[ligne + 1][colonne] == EtatCase.LIBRE || table[ligne + 1][colonne] == joueur.getColor())) {
             return true;  // Déplacement possible vers le bas
         }
-
-        /**
-         *
-         */
         if (colonne >= 0 && colonne < taille_col - 1 && ligne >= 0 && ligne < taill_ligne &&
                 (table[ligne][colonne] == EtatCase.LIBRE || table[ligne][colonne] == joueur.getColor()) &&
                 (table[ligne][colonne + 1] == EtatCase.LIBRE || table[ligne][colonne + 1] == joueur.getColor())) {
@@ -42,13 +49,11 @@ public class MouvementsJoueurs {
                 (table[ligne - 1][colonne] == EtatCase.LIBRE || table[ligne - 1][colonne] == joueur.getColor())) {
             return true;  // Déplacement possible vers le haut
         }
-
         if (colonne > 0 && colonne < taille_col && ligne >= 0 && ligne < taill_ligne &&
                 (table[ligne][colonne] == EtatCase.LIBRE || table[ligne][colonne] == joueur.getColor()) &&
                 (table[ligne][colonne - 1] == EtatCase.LIBRE || table[ligne][colonne - 1] == joueur.getColor())) {
             return true;  // Déplacement possible vers la gauche
         }
-
         return false;
     }
 
@@ -65,26 +70,35 @@ public class MouvementsJoueurs {
 
     /**
      * Permet de numérotter sur la case les déplacement possible
+     *
      * @param table : table de jeu
+     * @param joueur : le joueur dont on souhaite calculer les déplacement possibles
      */
-    public static int[][] calcDeplacementPossible(EtatCase[][] table, Joueur joueur) {
-        combiPossible = new int[compteMvtPossibles(table, joueur)][4]; // tableau à deux dimensions
-
+    public static int[][][] calcDeplacementPossible(EtatCase[][] table, Joueur joueur) {
+        listMouvement = new int[compteMvtPossibles(table, joueur)][2][2];
         int compteur = 0;
         for (int i = 0; i < table.length; i++) {
+            //TODO debug ici : j doit etre adjacent a i
             for (int j = 0; j < table[0].length; j++) {
                 if (isMvtPossible(table, i, j, joueur)) {
                     // Retenir le couple du mouvement possible sous forme ([0][0], [0][1])
-                    combiPossible[compteur][0] = i; // ligne1
-                    combiPossible[compteur][1] = j; // colonne1
-                    combiPossible[compteur][2] = i + 1; // ligne2
-                    combiPossible[compteur][3] = j; // colonne2
+                    listMouvement[compteur][0][0] = i; // ligne1
+                    listMouvement[compteur][0][1] = j; // colonne1
+                    //TODO debug ici
+                    if(combiExist(table, i, j,i+1,j, joueur)){
+                        listMouvement[compteur][1][0] = i+1; // ligne2
+                        listMouvement[compteur][1][1] = j; // colonne2
+                    }
+                    if(combiExist(table, i, j,i,j+1, joueur)){
+                        listMouvement[compteur][1][0] = i; // ligne2
+                        listMouvement[compteur][1][1] = j+1; // colonne2
+                    }
                     ++compteur;
                 }
             }
         }
 
-        return combiPossible;
+        return listMouvement;
     }
 
     /**
@@ -93,10 +107,8 @@ public class MouvementsJoueurs {
      */
     public static void deplacerPiece(EtatCase[][] table, Joueur joueur) {
         // afficher table des mouvements possibles
-
-
         Scanner sc = new Scanner(System.in);
-        printCombi(calcDeplacementPossible(table, joueur));  // Utilise la nouvelle fonction pour afficher les combinaisons
+        afficherDeplacements(calcDeplacementPossible(table, joueur));  // Utilise la nouvelle fonction pour afficher les combinaisons
         System.out.print("Quel déplacement souhaitez-vous faire ? ");
         String resultat = sc.next();
         int numero = Integer.parseInt(resultat);
@@ -110,13 +122,14 @@ public class MouvementsJoueurs {
         }
         System.out.println("Numéro invalide !");
     }
-    public static void afficherDeplacements(EtatCase[][] table) {
+    public static void afficherDeplacements(int[][][] tableau) {
         System.out.println("Couples de déplacements possibles : ");
-        for (int i = 0; i < mouvementsPossibles.length; ++i) {
-            int ligne1 = mouvementsPossibles[i][0];
-            int colonne1 = mouvementsPossibles[i][1];
+        for (int i = 0; i < tableau.length; ++i) {
+            System.out.println("([" + tableau[i][0][0] + "][" + tableau[i][0][1] + "], [" + tableau[i][1][0] + "][" + tableau[i][1][1] + "])");
+            //int ligne1 = mouvementsPossibles[i][0];
+            //int colonne1 = mouvementsPossibles[i][1];
             // Vérifier si le déplacement vers le bas (ligne+1) est possible
-            if (ligne1 < table.length - 1 && table[ligne1 + 1][colonne1] == EtatCase.LIBRE) {
+            /*if (ligne1 < table.length - 1 && table[ligne1 + 1][colonne1] == EtatCase.LIBRE) {
                 int ligne2 = ligne1 + 1;
                 int colonne2 = colonne1;
                 System.out.println("([" + ligne1 + "][" + colonne1 + "], [" + ligne2 + "][" + colonne2 + "])");
@@ -138,22 +151,7 @@ public class MouvementsJoueurs {
                 int ligne2 = ligne1 - 1;
                 int colonne2 = colonne1;
                 System.out.println("([" + ligne1 + "][" + colonne1 + "], [" + ligne2 + "][" + colonne2 + "])");
-            }
-
-
+            }*/
         }
     }
-    public static void printCombi(int[][] combiPossible) {
-        System.out.println("Couples de déplacements possibles : ");
-        for (int i = 0; i < combiPossible.length; ++i) {
-            int ligne1 = combiPossible[i][0]; // case 1
-            int colonne1 = combiPossible[i][1]; // case 1
-            int ligne2 = combiPossible[i][2]; // case 2
-            int colonne2 = combiPossible[i][3]; // case 2
-            System.out.println("([" + ligne1 + "][" + colonne1 + "], [" + ligne2 + "][" + colonne2 + "])");
-        }
-    }
-
-
-
 }
